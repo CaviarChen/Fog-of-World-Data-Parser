174fc5e4bfa8f729995266ff8ab2bb266d2c6d8e
Zed(Zijun) Chen <c.zijunchen@gmail.com>
2021-08-10 08:24:12 -0700

    Small refactor.

--- parser.py
+++ parser.py
@@ -6,27 +6,30 @@ import zlib
 import struct
 
 
+FILENAME_ENCODING = {k: v for v, k in enumerate("olhwjsktri")}
+MAP_WIDTH  = 512
+TILE_WIDTH = 128
+TILE_HEADER_LEN = TILE_WIDTH**2
+TILE_HEADER_SIZE = TILE_HEADER_LEN*2
+
 class Tile():
-    FILENAME_ENCODING = {k: v for v, k in enumerate("olhwjsktri")}
-    HEADER_LEN = 128*128
-    HEADER_SIZE = HEADER_LEN*2
 
     def __init__(self, sync_folder, filename):
         file = os.path.join(sync_folder, filename)
         # parse filename
         # TODO: figure out what the rest part of the filename is
         self.id = 0
-        for v in [self.FILENAME_ENCODING[c] for c in filename[4:10]]:
+        for v in [FILENAME_ENCODING[c] for c in filename[4:10]]:
             self.id = self.id * 10 + v
-        self.x = self.id % 512
-        self.y = self.id // 512
+        self.x = self.id % MAP_WIDTH
+        self.y = self.id // MAP_WIDTH
         print("Loading tile. id: {}, x: {}, y: {}".format(self.id, self.x, self.y))
         with open(file, "rb") as f:
             data = f.read()
             data = zlib.decompress(data)
         # header is a 2d array of shorts, it contains the maping of blocks
-        header = struct.unpack(str(self.HEADER_LEN) +
-                               "H", data[:self.HEADER_SIZE])
+        header = struct.unpack(str(TILE_HEADER_LEN) +
+                               "H", data[:TILE_HEADER_SIZE])
 
 
 class FogMap():
