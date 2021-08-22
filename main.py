7f3ba6ce34e7149dd46da14ce2056c290572a6c9
Zed(Zijun) Chen <c.zijunchen@gmail.com>
2021-08-22 07:41:19 -0700

    Speedup image generation.

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import click
 import folium
 import tempfile
 import atexit
+import itertools
 from http.server import HTTPServer
+from multiprocessing import Pool
 from PIL import Image, ImageColor
 import parser
 
 ZOOM_LEVEL = 4
+
 
 def serve_http(port, folder):
     class Handler(http.server.SimpleHTTPRequestHandler):
@@ -17,6 +20,22 @@ def serve_http(port, folder):
     http_server = HTTPServer(("", port), Handler)
     print("http://127.0.0.1:{}/index.html".format(port))
     http_server.serve_forever()
+
+
+def generate_image(tile, tmp_folder):
+    print("generating image for tile: {}".format(tile.id))
+    size = parser.TILE_WIDTH*parser.BITMAP_WIDTH // ZOOM_LEVEL
+    im = Image.new("RGBA", (size, size), (255, 255, 255, 0))
+    for block in tile.blocks.values():
+        for i in range(parser.BITMAP_WIDTH):
+            for j in range(parser.BITMAP_WIDTH):
+                x = block.x * parser.BITMAP_WIDTH + i
+                y = block.y * parser.BITMAP_WIDTH + j
+                x //= ZOOM_LEVEL
+                y //= ZOOM_LEVEL
+                if block.is_visited(i, j):
+                    im.putpixel((x, y), ImageColor.getcolor('black', 'RGB'))
+    im.save(os.path.join(tmp_folder.name, "{}.png".format(tile.id)))
 
 
 @click.command()
@@ -30,26 +49,18 @@ def main(port, dir):
     fog_map = parser.FogMap(dir)
 
     tmp_folder = tempfile.TemporaryDirectory()
+
     def exit_handler():
         tmp_folder.cleanup()
     atexit.register(exit_handler)
 
     m = folium.Map()
+    with Pool(4) as pool:
+        pool.starmap(generate_image, zip(fog_map.tile_map.values(), itertools.repeat(tmp_folder)))
+
     for tile in fog_map.tile_map.values():
-        print("generating image for tile: {}".format(tile.id))
-        size = parser.TILE_WIDTH*parser.BITMAP_WIDTH // ZOOM_LEVEL
-        im = Image.new("RGBA", (size, size), (255, 255, 255, 0))
-        for block in tile.blocks.values():
-            for i in range(parser.BITMAP_WIDTH):
-                for j in range(parser.BITMAP_WIDTH):
-                    x = block.x * parser.BITMAP_WIDTH + i
-                    y = block.y * parser.BITMAP_WIDTH + j
-                    x //= ZOOM_LEVEL
-                    y //= ZOOM_LEVEL
-                    if block.is_visited(i, j):
-                        im.putpixel((x, y), ImageColor.getcolor('black','RGB'))
-        im.save(os.path.join(tmp_folder.name, "{}.png".format(tile.id)))
-        folium.raster_layers.ImageOverlay("http://127.0.0.1:{}/{}.png".format(port, tile.id), tile.bounds(), opacity=1).add_to(m)
+        folium.raster_layers.ImageOverlay(
+            "http://127.0.0.1:{}/{}.png".format(port, tile.id), tile.bounds(), opacity=1).add_to(m)
         print(tile.bounds())
 
     m.save(os.path.join(tmp_folder.name, "index.html"))
